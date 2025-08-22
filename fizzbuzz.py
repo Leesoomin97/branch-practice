c30d7c600045aa35990724252032cb917e9c82d7
soomin97 <milpasoomin@gmail.com>
2025-08-22 11:17:51 +0900

    test: mc test on branch mc

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,8 +1,10 @@
-for i in range(1, 16+1):
+for j in range(10, 20+1):
     if i % 3 == 0:
         print('fizz')
+    elif j % 5 == 0:
+        print('buzz')
     else:
-        print('i')
+        print(j)
 
 
 
